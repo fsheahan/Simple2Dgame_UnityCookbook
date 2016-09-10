7153511c0293c21636c66d91c08f614d84f7e189
fsheahan <fred@primeacademy.io>
2016-09-09 22:06:03 -0500

    Unity 5X Cookbok chapter 2.5 halfway

--- Assets/SpaceGirlMiniGame/Scripts/Player.cs
+++ Assets/SpaceGirlMiniGame/Scripts/Player.cs
@@ -2,20 +2,16 @@
 using System.Collections;
 using UnityEngine.UI;
 public class Player : MonoBehaviour {
-	public Text starText;
+	private PlayerInventoryDisplay playerInventoryDisplay;
 	private int totalStars = 0;
 	void Start(){
-		UpdateStarText();
-	}
+		playerInventoryDisplay = GetComponent<PlayerInventoryDispl
+			ay>(); }
 	void OnTriggerEnter2D(Collider2D hit){
 		if(hit.CompareTag("Star")){
 			totalStars++;
-			UpdateStarText();
+			playerInventoryDisplay.OnChangeStarTotal(totalStars);
 			Destroy(hit.gameObject);
-		} 
-	}
-	private void UpdateStarText(){
-		string starMessage = "stars = " + totalStars;
-		starText.text = starMessage;
+		}
 	} 
 }
--- Assets/SpaceGirlMiniGame/Scripts/PlayerInventoryDisplay.cs
+++ Assets/SpaceGirlMiniGame/Scripts/PlayerInventoryDisplay.cs
@@ -3,10 +3,15 @@ using System.Collections;
 using UnityEngine.UI;
 public class PlayerInventoryDisplay : MonoBehaviour
 {
-	public Text starText;
-	public void OnChangeCarryingStar(bool carryingStar){
-		string starMessage = "no star :-(";
-		if(carryingStar) starMessage = "Carrying star :-)";
-		starText.text = starMessage;
+	public Image[] starPlaceholders;
+		public Sprite iconStarYellow;
+	public Sprite iconStarGrey;
+	public void OnChangeStarTotal(int starTotal){
+		for (int i = 0;i < starPlaceholders.Length; ++i){
+			if (i < starTotal)
+				starPlaceholders[i].sprite = iconStarYellow;
+			else
+				starPlaceholders[i].sprite = iconStarGrey;
+		}
 	} 
 }
