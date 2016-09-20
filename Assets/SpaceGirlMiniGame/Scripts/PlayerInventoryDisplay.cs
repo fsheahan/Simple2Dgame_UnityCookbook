30c458059a4bff669a9bd0e269a1508fba9d381d
fsheahan <fred@primeacademy.io>
2016-09-19 20:59:39 -0500

    Unity 5X Cookbok chapter 2.9 complete

--- Assets/SpaceGirlMiniGame/Scripts/PlayerInventoryDisplay.cs
+++ Assets/SpaceGirlMiniGame/Scripts/PlayerInventoryDisplay.cs
@@ -4,7 +4,18 @@ using UnityEngine.UI;
 public class PlayerInventoryDisplay : MonoBehaviour
 {
 	const int NUM_INVENTORY_SLOTS = 10;
-	public PickupUI[] slots = new PickupUI[NUM_INVENTORY_SLOTS];
+	private PickupUI[] slots = new PickupUI[NUM_INVENTORY_SLOTS];
+	public GameObject slotGrid;
+	public GameObject starSlotPrefab;
+	void Awake(){
+		for(int i=0; i < NUM_INVENTORY_SLOTS; i++){
+			GameObject starSlotGO = (GameObject)
+				Instantiate(starSlotPrefab);
+			starSlotGO.transform.SetParent(slotGrid.transform);
+			starSlotGO.transform.localScale = new Vector3(1,1,1);
+			slots[i] = starSlotGO.GetComponent<PickupUI>();
+		} 
+	}
 	public void OnChangeStarTotal(int starTotal){
 		for(int i = 0; i < NUM_INVENTORY_SLOTS; i++){
 			PickupUI slot = slots[i];
@@ -13,5 +24,13 @@ public class PlayerInventoryDisplay : MonoBehaviour
 			else
 				slot.DisplayGrey();
 		}
-	} 
+	}
+	void Start(){
+		float panelWidth = slotGrid.GetComponent<RectTransform>().rect.width;
+		print ("slotGrid.GetComponent<RectTransform>().rect = " + slotGrid.GetComponent<RectTransform>().rect); 
+		GridLayoutGroup gridLayoutGroup = slotGrid.GetComponent<GridLayoutGroup>();
+		float xCellSize = panelWidth / NUM_INVENTORY_SLOTS;
+		xCellSize -= gridLayoutGroup.spacing.x;
+		gridLayoutGroup.cellSize = new Vector2(xCellSize, xCellSize);
+	}
 }
